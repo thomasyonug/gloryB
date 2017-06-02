884946541a8c10a3fe5516f4fe3e876c7afc2682
thomasyonug <563473579@qq.com>
2017-06-02 10:22:15 +0800

    ws

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const Koa = require('koa')
 const app = new Koa()
 const config = require('./config')
 const port = config.dev.port
-
+const WebSocket = require(config.path.websocket)
 const middlewares = require(config.path.middleware)
 const api = require(config.path.api)
 const public = require(config.path.public)
-// app.use()
+
+
+
 
 
 middlewares.forEach(middleware => {
@@ -18,8 +20,15 @@ app.use(api.middleware())
 app.use(public.middleware())
 
 
+
+
+
+
+
+
 try{ 
-    app.listen(port)
+    const server = app.listen(port)
+    new WebSocket({app, server}).init()
     console.log(`listening on: ${port}`)
 }catch(err){
     console.log(`boot error: ${err}`)
