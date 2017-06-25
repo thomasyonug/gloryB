d67a73aa00c0493c7714a849961b455fe509b4b1
thomasyonug <563473579@qq.com>
2017-06-25 15:44:05 +0800

    add sub controller in rootController

--- src/websocket/rootController/index.js
+++ src/websocket/rootController/index.js
@@ -1,10 +1,45 @@
 const decoratorsPath = load('config').path.decorators
 const classMeta = require(decoratorsPath).classMeta
+const RoomController = require('./roomController')
+const GameController = require('./gameController')
+
 
 module.exports = 
     @classMeta
     class RootController{
-        constructor(arg){
+        io;
+        room;
+        game;
+        wsClass;
+        gameCtrler;
+        roomCtrler;
 
+        constructor({io, room, game, wsClass}){
+            Object.assign(this, {
+                io, 
+                room, 
+                game, 
+                wsClass,
+                gameCtrler: new GameController(),
+                roomCtrler: new RoomController()
+            })
         } 
+
+
+
+        connect (socket) {
+            this.room.loginCustomer(socket)
+        }
+
+
+        roomController (msg) {
+            this.gameCtrler.on(msg)
+        }
+
+        gameController (msg) {
+            this.roomCtrler.on(msg)
+        }
+
+        
+
     }
