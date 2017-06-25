7dc04f65f46c4f8b80838bc0ad930695cfa471a6
thomasyonug <563473579@qq.com>
2017-06-25 15:43:34 +0800

    Room class only support room store api, divide socket listener

--- src/websocket/room/index.js
+++ src/websocket/room/index.js
@@ -1,4 +1,4 @@
-const RoomStore = require('./roomCoreClass')
+const RoomCore = require('./core')
 const classMeta = require(load('config').path.decorators).classMeta
 
 
@@ -7,82 +7,21 @@ const classMeta = require(load('config').path.decorators).classMeta
 module.exports = 
     @classMeta
     class Room{
+        io;
+        socket;
+        core;
         constructor({io, socket}){
-            this.io = io
-            this.roomStore = new RoomStore()
-        }
-        init () {
-            this.io.on('connection', (socket) => {
-                this.roomStore.joinOutside(socket)
-
-
-                // socket.on('room', msg => )
-
-                // socket.on('createRoom', msg => this.createRoomHandle(socket, msg))
-                // socket.on('getRooms', msg => this.getRoomsHandle(socket, msg))
-                // socket.on('joinRoom', msg => this.joinRoomHandle(socket, msg))
-                // socket.on('leaveRoom', msg => this.leaveRoomHandle(socket, msg))
-
-                // //native event
-                // socket.on('disconnect', msg => this.disconnectHandle(socket, msg))
+            Object.assign(this, {
+                io,
+                socket,
+                roomCore: new RoomCore()
             })
         }
 
 
-        createRoomHandle (socket, msg) {
-            const {name} = msg
-            this.roomStore.create(name, socket)
-            this.joinRoomHandle(socket, {
-                roomID: socket.adapter._room
-            })
-        }
-
-        getRoomsHandle (socket, msg) {
-            socket.emit('getRooms', this.roomStore.getAsArray())
-        }
-
-        joinRoomHandle (socket, msg) {
-            const {roomID} = msg
-            this.roomStore.join(roomID, socket)
-            this.io.to(roomID).emit('joinRoom', this.roomStore.store[roomID])
-        }
-
-        leaveRoomHandle (socket, msg) {
-            const roomID = socket.adapter._room
-            this.roomStore.leave(socket)
-            this.io.to(roomID).emit('joinRoom', this.roomStore.store[roomID])
-        }
-
-
-        disconnectHandle (socket, msg) {
-            this.roomStore.offline(socket)
-        }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        updateRoomAllMember (roomID) {
-        }
-
-
-        
+        loginCustomer (socket) {
+            this.roomCore.login(socket)
+        } 
     }
 
 
