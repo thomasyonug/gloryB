68e22848873a6f8ccffa7fc83d92217063a9738b
thomasyonug <563473579@qq.com>
2017-06-26 00:21:04 +0800

    modify little

--- src/websocket/rootController/gameController/index.js
+++ src/websocket/rootController/gameController/index.js
@@ -4,7 +4,10 @@ const classMeta = require(load('config').path.decorators).classMeta
 module.exports = 
     @classMeta
     class GameController {
-        constructor () {
+        game;
+
+
+        constructor ({game}) {
 
         }
 
--- src/websocket/rootController/roomController/index.js
+++ src/websocket/rootController/roomController/index.js
@@ -3,7 +3,9 @@ const classMeta = require(load('config').path.decorators).classMeta
 module.exports = 
     @classMeta
     class RoomController {
-        constructor () {
+        room;
+
+        constructor ({room}) {
 
         }
 
