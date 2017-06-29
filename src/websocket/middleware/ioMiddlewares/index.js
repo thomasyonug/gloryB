7f261745e42175da23ef983753f7e1a55ca6b5cf
thomasyonug <563473579@qq.com>
2017-06-29 23:28:19 +0800

    add io middleware to decorate socket $emit method，so I can hook the native emit

--- src/websocket/middleware/ioMiddlewares/index.js
+++ src/websocket/middleware/ioMiddlewares/index.js
@@ -1,5 +1,18 @@
+const chalk = require('chalk')
+
+let _emit = null
+
+const $emit = function (...ev) {
+    console.log(chalk.green('send ---', ...ev))
+    return _emit.call(this, ...ev)
+}
+
 module.exports = [
-    function testMiddle () {
-        console.log(132, 'middleware')
+    function socketDecorator (socket, next) {
+        if (_emit === null) _emit = socket.emit
+
+        socket.$emit = $emit
+
+        return next()
     }
 ]
