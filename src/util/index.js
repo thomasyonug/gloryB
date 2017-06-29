067ad116c758dbb184027d4ad41931aa74313aae
thomasyonug <563473579@qq.com>
2017-06-29 23:24:39 +0800

    add error handler in util

--- src/util/index.js
+++ src/util/index.js
@@ -1,10 +1,12 @@
 const helper = require('./helper')
 const log    = require('./log')
 const config = require('./config')
+const error    = require('./error')
 const store = {
     helper,
     log,
-    config
+    config,
+    error
 }
 
 global.load = field => {
