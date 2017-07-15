7c1042d838f9cb09434a200ee456e3c2e0989552
wx-5161 <wx-yesf@dtdream.com>
2017-07-15 11:03:50 +0800

    add usingGroup

--- src/models/index.js
+++ src/models/index.js
@@ -1,17 +1,13 @@
 const mongoose = require('mongoose')
 
-
 mongoose.Promise = global.Promise
 
-mongoose.connect('mongodb://localhost:27017/glory',function(err){
-	if(err){
-        throw new Error(err)
-		process.exit(1)
-	}
-	console.log('connect success')
+mongoose.connect('mongodb://123.206.180.56:27017/glory', function (err) {
+  if (err) {
+    throw new Error(err)
+    process.exit(1)
+  }
+  console.log('connect success')
 })
-
-
-
 
 exports.userModel = require('./user')
--- src/models/user.js
+++ src/models/user.js
@@ -1,20 +1,21 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-
-
 const user = new Schema({
-	username: String,
-	password: String,
-	arrengement: {
-		cardGroups: [{
-			groupName: String,
-			cards: [{
-				cardCode: String
-			}]
-		}]
-	},
-	create_date: Date
+  username: String,
+  password: String,
+  arrengement: {
+    cardGroups: [{
+      groupName: String,
+      cards: [{
+        cardCode: String
+      }]
+    }],
+    usingGroup: {
+      groupName: String
+    }
+  },
+  create_date: Date
 })
 
 module.exports = mongoose.model('user', user)
