9e1139e7b5a05328437d6181ac6526d923feafac
joursion <joursion@126.com>
2017-08-28 23:28:41 +0800

    modify the lib/friends.js

--- src/lib/friends.js
+++ src/lib/friends.js
@@ -1,20 +1,23 @@
 const userModel = require('../models').userModel
 
 
-async function addFriend(_id, user) {
-    const userObj = userModel.findById(_id)
-    userObj.friends.push(user)
-    return userObj.save()
+async function addFriend(_id, friend) {
+    const isFirend = await userModel.findOne({ _id: _id, 'friends.username': friend.username });
+    if (isFirend) {
+        return null;
+    }
+    return await userModel.update({ _id: _id }, { $push: { friends: friend } });
 }
 async function deleteFriend(_id, username) {
-    const user = userModel.findById(_id)
-    user.friends = user.friends.$delete(friend => friend.username === username)
-    return user.save()
+    return await userModel.update({ _id: _id }, { $pull: { friends: { username: username } } });
+    // const user = userModel.findById(_id)
+    // user.friends = user.friends.$delete(friend => friend.username === username)
+    // return user.save()
 }
 
 async function getFriends(_id) {
-    const users = await userModel.findById(_id)
-    return users.friends
+    const users = await userModel.findOne({ _id: _id }, { friends: 1 });
+    return users.friends;
 }
 
 
